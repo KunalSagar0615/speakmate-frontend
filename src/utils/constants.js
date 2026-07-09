0e8d600272ba995b82cea0eb0ca6bf95e5130cec
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2026-07-09 12:43:03 +0530

    first commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://speakmate-ai-friend.onrender.com";
 export const TOKEN_KEY = "speakmate_token";
 export const ROLE_KEY = "speakmate_role";
 export const USER_KEY = "speakmate_user";
